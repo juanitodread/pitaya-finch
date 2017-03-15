20d0b40617a425581f8c5e653373294341a2bc14
Juan Sandoval <juanitodread@gmail.com>
2017-03-15 02:25:22 -0600

    First commit

---
+++ build.sbt
@@ -0,0 +1,12 @@
+name := "pitaya-finch"
+version := "0.0.1"
+
+lazy val root = (project in file("."))
+
+scalaVersion := "2.11.8"
+
+libraryDependencies ++= Seq(
+  "com.github.finagle" %% "finch-core" % "0.13.1",
+  "com.github.finagle" %% "finch-circe" % "0.13.1",
+  "io.circe" %% "circe-generic" % "0.7.0"
+)
---
+++ src/main/scala/org/juanitodread/pitayafinch/App.scala
@@ -0,0 +1,19 @@
+package org.juanitodread.pitayafinch
+
+import com.twitter.finagle.Http
+import com.twitter.util.Await
+
+import io.finch._
+import io.finch.circe._
+
+object App {
+  def main(args: Array[String]): Unit = {
+    println("Hello World")
+
+    val api0: Endpoint[String] = get("hello") { Ok("Hello, World!") }
+    val api1: Endpoint[String] = get("hello1") { Ok("Hello, World!") }
+    val api = api0 :+: api1
+
+    Await.ready(Http.serve(":8989", api.toService))
+  }
+}

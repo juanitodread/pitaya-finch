42a44f443f38268734a6f69610ed5b81c427d712
juanitodread <juanitodread@gmail.com>
2019-04-04 16:30:38 -0700

    Add tests for getUser and postUser

--- src/test/scala/org/juanitodread/pitayafinch/routes/cookbook/UsersTest.scala
+++ src/test/scala/org/juanitodread/pitayafinch/routes/cookbook/UsersTest.scala
@@ -2,11 +2,10 @@ package org.juanitodread.pitayafinch.routes.cookbook
 
 import java.util.UUID
 
-import io.finch.Input
+import io.finch.{ Endpoint, Input }
 import io.finch.Application.Json
 import io.finch.circe._
 import io.circe.generic.auto._
-
 import org.juanitodread.pitayafinch.UnitSpec
 import org.juanitodread.pitayafinch.model.{ User => UserModel }
 
@@ -28,12 +27,24 @@ class UsersTest extends UnitSpec {
 
   "A Users route" should "have getUsers endpoint" in {
     prepareUsers
-
     Users.getUsers.apply(Input.get(baseApi)).awaitValueUnsafe().get.map { user =>
       (user.name, user.age, user.address, user.roles)
     } should contain theSameElementsAs List(
       ("Juan", 10, None, List()),
       ("Antonio", 11, None, List("admin")))
+  }
+
+  it should "have getUser endpoint" in {
+    prepareUsers
+    Users.getUser.apply(Input.get(baseApi + "/none")).awaitValueUnsafe() shouldEqual None
+  }
+
+  it should "have postUser endpoint" in {
+    prepareUsers
+    val user = UserModel(UUID.fromString("7becf740-7f1e-4cf4-b908-d67397b3503f"), "Peter", 22, None, List())
+    Users.postUser.apply(Input.post(baseApi).withBody[Json](user)).awaitValueUnsafe() should matchPattern {
+      case Some(UserModel(_, "Peter", 22, None, List())) =>
+    }
   }
 
 }

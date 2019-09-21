e8c6ec5dff18321c9d2e06e46a530ecf787bce32
Juan Sandoval <juanitodread@gmail.com>
2019-09-20 17:55:33 -0700

    Fix index page message (#41)

--- src/main/scala/org/juanitodread/pitayafinch/routes/Index.scala
+++ src/main/scala/org/juanitodread/pitayafinch/routes/Index.scala
@@ -1,17 +1,19 @@
 package org.juanitodread.pitayafinch.routes
 
 import cats.effect.IO
+import io.circe.Json
+import io.circe.syntax._
 import io.finch._
 import io.finch.catsEffect._
 
 object Index extends BaseEndpoint {
 
-  def index(): Endpoint[IO, String] = get(basePath) {
-    Ok("Hello World")
+  def index(): Endpoint[IO, Json] = get(basePath) {
+    Ok(Map(
+      "message" -> "Please check the API documentation here: https://github.com/juanitodread/pitaya-finch#api-definition").asJson)
   }
 
   def help(): Endpoint[IO, String] = get(basePath :: "help") {
     Ok("This is the help for root")
   }
-
 }
--- src/test/scala/org/juanitodread/pitayafinch/routes/IndexTest.scala
+++ src/test/scala/org/juanitodread/pitayafinch/routes/IndexTest.scala
@@ -1,5 +1,6 @@
 package org.juanitodread.pitayafinch.routes
 
+import io.circe.Json
 import io.finch._
 
 import org.juanitodread.pitayafinch.UnitSpec
@@ -8,8 +9,10 @@ class IndexTest extends UnitSpec {
   private val baseApi = "/pitaya/api/v1"
 
   "An Index route" should "have an index endpoint" in {
-    assert(Index.index.apply(
-      Input.get(baseApi)).awaitValueUnsafe().contains("Hello World"))
+    Index.index.apply(
+      Input.get(baseApi)).awaitValueUnsafe().get shouldEqual {
+        Json.obj("message" -> Json.fromString("Please check the API documentation here: https://github.com/juanitodread/pitaya-finch#api-definition"))
+      }
   }
 
   it should "have a help endpoint" in {

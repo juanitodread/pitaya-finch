6392f4993d9877746df22bef95a66682c5bd4cfe
juanitodread <juanitodread@gmail.com>
2019-09-23 15:23:02 -0700

    Add tests for Finder.round method

--- src/main/scala/org/juanitodread/pitayafinch/nlp/tools/sentences/Finder.scala
+++ src/main/scala/org/juanitodread/pitayafinch/nlp/tools/sentences/Finder.scala
@@ -19,7 +19,7 @@ class Finder(modelPath: String) {
     (sentences zip confidences).map { result =>
       FinderResult(
         sentence = result._1,
-        confidence = Finder.formatConfidence(result._2))
+        confidence = Finder.round(result._2))
     }
   }
 }
@@ -32,7 +32,7 @@ object Finder {
     finder.find(text)
   }
 
-  def formatConfidence(confidence: Double, precision: Int = 3): Double = {
+  def round(confidence: Double, precision: Int = 3): Double = {
     require(precision > 0 && precision < 6)
     val decimals = Math.pow(10, precision)
     Math.rint(confidence * decimals) / decimals
--- src/test/scala/org/juanitodread/pitayafinch/nlp/tools/sentences/FinderSpec.scala
+++ src/test/scala/org/juanitodread/pitayafinch/nlp/tools/sentences/FinderSpec.scala
@@ -32,4 +32,34 @@ class FinderSpec extends UnitSpec {
       "Within sentences we may find numbers like 3.14159, abbreviations such as found in Mr. Smith, " +
         "and possibly ellipses either within a sentence ..., or at the end of a sentence..."))
   }
+
+  "A Finder object" should "format the confidence result number with default precision" in {
+    assert(Finder.round(0.123456789) === 0.123)
+  }
+
+  it should "format the confidence result number with min precision number allowed" in {
+    assert(Finder.round(0.123456789, 1) === 0.1)
+  }
+
+  it should "format the confidence result number with max precision number allowed" in {
+    assert(Finder.round(0.123456789, 5) === 0.12346) // 5 rounds to 6
+  }
+
+  it should "fail when zero precision value is provided" in {
+    assertThrows[IllegalArgumentException] {
+      Finder.round(0.123456789, 0)
+    }
+  }
+
+  it should "fail when negative precision value is provided" in {
+    assertThrows[IllegalArgumentException] {
+      Finder.round(0.123456789, -1)
+    }
+  }
+
+  it should "fail when precision value provided is greater than max allowed" in {
+    assertThrows[IllegalArgumentException] {
+      Finder.round(0.123456789, 5 + 1)
+    }
+  }
 }
